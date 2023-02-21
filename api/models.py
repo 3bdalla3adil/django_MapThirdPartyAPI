7d04b1332050ed18cb1a1346d423f1455c25be17
3bdalla3adil <3bdalla995@gmail.com>
2023-02-21 17:00:18 +0200

    added Transient Model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class TransientModel(models.Model):
+    def save(*args,**kwargs):
+        pass
+    class Meta:
+        abstract = True
+        managed = False
+
+class ThirdPartyAPI(TransientModel):
+    origin = TransientModel.TextField("Origin")
